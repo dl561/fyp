15d4a7cc0776f9300292c44b499565a6152296e6
David Lambeth <dl561@bath.ac.uk>
2017-12-09 11:55:14 +0000

    added docs folder

---
+++ docs/js/options.js
@@ -0,0 +1,62 @@
+function submitFunction() {
+	var carCount = document.getElementById("carCount").value;
+	var trackNumber1 = document.getElementById("trackNumber1").checked;
+	var trackNumber2 = document.getElementById("trackNumber2").checked;
+	var sound = document.getElementById("soundCheckbox").checked;
+	var hostIp = document.getElementById("ipAddressBox").value;
+	if (hostIp == "") {
+		hostIp = "localhost";
+	}
+	var localCarNumber = getLocalCarNumber();
+	var computerCars = [];
+	for (i = 1; i <= carCount; i++) {
+		if (document.getElementById("carComputer" + i).checked) {
+			computerCars[i] = 1;
+		} else {
+			computerCars[i] = 0;
+		}
+	}
+	if (!trackNumber1 && !trackNumber2) {
+		var errorLabel = document.getElementById("trackNumberError");
+		errorLabel.hidden = false;
+		return;
+	} else {
+		var buttons = document.getElementById("submit");
+		var newUrl = "web.html";
+		localStorage.setItem("carCount", carCount);
+		localStorage.setItem("trackNumber1", trackNumber1);
+		localStorage.setItem("trackNumber2", trackNumber2);
+		localStorage.setItem("sound", sound);
+		localStorage.setItem("hostIP", hostIp);
+		localStorage.setItem("localCarNumber", localCarNumber);
+		localStorage.setItem("computerCars", computerCars);
+		window.location.replace(newUrl);
+		document.location.href = newUrl;
+	}
+}
+function updateRange() {
+	var range = document.getElementById("carCount").value;
+	var label = document.getElementById("carCountLabel");
+	label.innerHTML = range;
+	hideCarButtons(range);
+}
+function checkSingleLocal(carNumber) {
+	if (document.getElementById("carLocal" + carNumber).checked) {
+		for (i = 1; i < 7; i++) {
+			if (i != carNumber) {
+				var x = document.getElementById("carLocal" + i);
+				x.checked = false;
+			}
+		}
+	}
+}
+
+function getLocalCarNumber() {
+	var localCar = -1;
+	for (i = 1; i <= document.getElementById("carCount").value; i++) {
+		if (document.getElementById("carLocal" + i).checked) {
+			localCar = i;
+		}
+	}
+	return localCar;
+}
---
+++ docs/js/simulator.js
@@ -0,0 +1,259 @@
+var canvas = document.getElementById("simulationCanvas");
+var ctx = canvas.getContext("2d");
+
+var stopButton = document.getElementById("stop");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight - stopButton.offsetHeight;
+
+var simulationId;
+var tickCount = 0;
+
+var courseRectangles = [];
+var courseArcs = [];
+var vehicleObjects = [];
+var hUDObjects = [];
+
+var accelerating = false;
+var braking = false;
+var left = false;
+var right = false;
+
+var acceleratorPedalDepthVariable = 0;
+var brakePedalDepthVariable = 0;
+var steeringAngle = 0;
+var gearNumber = 1;
+var frontSlip = false;
+var rearSlip = false;
+
+var lastGearChange = 0;
+
+var localCarNumber = localStorage.getItem("localCarNumber");
+setHostIP(localStorage.getItem("hostIP"));
+
+initImages();
+
+var trackNumber1 = localStorage.getItem("trackNumber1");
+var trackNumber2 = localStorage.getItem("trackNumber2");
+var trackNumber;
+if (trackNumber1) {
+	trackNumber = 1;
+} else if (trackNumber2) {
+	trackNumber = 2;
+}
+localStorage.setItem("trackNumber", trackNumber);
+var carCount = localStorage.getItem("carCount");
+var computerCars = localStorage.getItem("computerCars");
+
+function populateCourseObjects(course) {
+	courseRectangles = course.rectangles;
+	courseArcs = course.arcs;
+}
+
+function populateVehicles(vehicles) {
+	vehicleObjects = vehicles;
+}
+
+function populateHUD(huds) {
+	hUDObjects = huds;
+}
+
+function populate(json) {
+	var obj = JSON.parse(json);
+	populateCourseObjects(obj.course);
+	populateVehicles(obj.vehicles);
+	populateHUD(obj.hud);
+}
+
+function startSimulation() {
+	console.log("Starting simulation");
+	doStartSimulation(function (response) {
+		//Add something here!
+	});
+}
+
+function stopSimulation() {
+	console.log("Stopping simulation");
+	doStopSimulation(function (response) {
+		//Add something here!
+	});
+}
+
+function resetSimulation() {
+	console.log("Resetting simulation");
+	doResetSimulation(function (response) {
+		console.log("Reset simulation " + response.id);
+	});
+}
+
+function newSimulation() {
+	console.log("Getting new simulation");
+	doNewSimulation(function (response) {
+		var responseObj = JSON.parse(response);
+		simulationId = responseObj.id;
+	});
+}
+
+function newSimulationByOptions() {
+	console.log("Getting new simulation by options");
+	console.log("Track Number: " + trackNumber);
+	console.log("Car count: " + carCount);
+	doNewSimulationByOptions(function (response) {
+		var responseObj = JSON.parse(response);
+		simulationId = responseObj.id;
+	}, trackNumber, getList());
+}
+
+function getList() {
+	var carList = [];
+	for (i = 0; i < carCount; i++) {
+		var car = new Object();
+		car.vehicleType = "CAR";
+		if (computerCars[1 + (2 * i)] == "1") {
+			car.isComputer = true;
+		} else {
+			car.isComputer = false;
+		}
+		carList.push(car);
+	}
+	console.log(carList);
+	return carList;
+}
+
+function fetch() {
+	doFetch(function (response) {
+		populate(response);
+	}, simulationId);
+}
+
+function fetchExample() {
+	doFetchExample(function (response) {
+		populate(response);
+		draw();
+	});
+}
+
+function sendUpdate() {
+	if (accelerating) {
+		acceleratorPedalDepthVariable += 10;
+	} else {
+		acceleratorPedalDepthVariable -= 10;
+	}
+	if (braking) {
+		brakePedalDepthVariable += 10;
+	} else {
+		brakePedalDepthVariable -= 10;
+	}
+
+	if (acceleratorPedalDepthVariable > 100) {
+		acceleratorPedalDepthVariable = 100;
+	}
+	if (acceleratorPedalDepthVariable < 0) {
+		acceleratorPedalDepthVariable = 0;
+	}
+	if (brakePedalDepthVariable > 100) {
+		brakePedalDepthVariable = 100;
+	}
+	if (brakePedalDepthVariable < 0) {
+		brakePedalDepthVariable = 0;
+	}
+
+	if (left) {
+		if (steeringAngle > -Math.PI / 6) {
+			steeringAngle -= Math.PI / 32;
+		}
+	} else if (right) {
+		if (steeringAngle < Math.PI / 6) {
+			steeringAngle += Math.PI / 32;
+		}
+	} else {
+		if (steeringAngle < 0) {
+			steeringAngle += Math.PI / 32;
+		} else if (steeringAngle > 0) {
+			steeringAngle -= Math.PI / 32;
+		}
+	}
+
+	var vehicleUpdateDto = new Object();
+	vehicleUpdateDto.id = localCarNumber;
+	vehicleUpdateDto.acceleratorPedalDepth = acceleratorPedalDepthVariable;
+	vehicleUpdateDto.brakePedalDepth = brakePedalDepthVariable;
+	vehicleUpdateDto.steeringWheelOrientation = steeringAngle;
+	vehicleUpdateDto.gear = gearNumber;
+
+	doUpdateVehicle(vehicleUpdateDto, simulationId, localCarNumber);
+}
+
+function updateKeys() {
+	var keys = getKeys();
+	if (keys[38]) {
+		accelerating = true;
+	} else {
+		accelerating = false;
+	}
+
+	if (keys[40]) {
+		braking = true;
+	} else {
+		braking = false;
+	}
+
+	if (keys[37]) {
+		left = true;
+	} else {
+		left = false;
+	}
+
+	if (keys[39]) {
+		right = true;
+	} else {
+		right = false;
+	}
+
+	if (keys[16]) {
+		//gear up with shft
+		if (gearNumber < 5 && tickCount - lastGearChange > 6) {
+			gearNumber++;
+			lastGearChange = tickCount;
+		}
+	}
+
+	if (keys[17]) {
+		//gear down with ctrl
+		if (gearNumber > 0 && tickCount - lastGearChange > 6) {
+			gearNumber--;
+			lastGearChange = tickCount;
+		}
+	}
+
+	if (keys[32]) {
+		frontSlip = true;
+		rearSlip = true;
+	} else {
+		frontSlip = false;
+		rearSlip = false;
+	}
+}
+
+function tick() {
+	tickCount++;
+	if (tickCount % 3) {
+		doUpdateTick();
+	}
+	fetch();
+	updateKeys();
+	draw(accelerating, braking, left, right, acceleratorPedalDepthVariable, brakePedalDepthVariable, steeringAngle, gearNumber);
+}
+
+function doUpdateTick() {
+	sendUpdate();
+}
+
+function beginTick() {
+	document.title = "Simulation number: " + simulationId;
+	playMusic(localStorage.getItem("sound"));
+	setInterval(tick, 30);
+}
+//CreateSimulationByOptions
+newSimulationByOptions();
+
+setTimeout(beginTick, 1000);
